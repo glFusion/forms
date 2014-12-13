b8e705a4071c39838f838d1931f3f5fbdc5932de
Lee Garner <lee@leegarner.com>
2014-12-13 11:41:08 -0800

    Bump Version

--- forms.php
+++ forms.php
@@ -5,7 +5,7 @@
 *   @author     Lee Garner <lee@leegarner.com>
 *   @copyright  Copyright (c) 2010-2014 Lee Garner <lee@leegarner.com>
 *   @package    forms
-*   @version    0.2.2
+*   @version    0.2.3
 *   @license    http://opensource.org/licenses/gpl-2.0.php 
 *               GNU Public License v2 or later
 *   @filesource
@@ -37,8 +37,8 @@ $_TABLES['forms_frmXfld']   = $_DB_table_prefix . 'forms_frmXfld';
 */
 global $_CONF_FRM;
 $_CONF_FRM['pi_name']           = 'forms';
-$_CONF_FRM['pi_version']        = '0.2.2';
-$_CONF_FRM['gl_version']        = '1.2.0';
+$_CONF_FRM['pi_version']        = '0.2.3';
+$_CONF_FRM['gl_version']        = '1.4.0';
 $_CONF_FRM['pi_url']            = 'http://www.leegarner.com';
 $_CONF_FRM['pi_display_name']   = 'Forms';
 
