67c09db50de1520df6f5e08fee3c4582758f0155
Lee Garner <lee@leegarner.com>
2022-03-10 16:45:51 -0800

    Leverage glFuion 2.x FieldList class

--- classes/FieldList.class.php
+++ classes/FieldList.class.php
@@ -20,7 +20,7 @@ namespace Forms;
  * Class to handle custom fields.
  * @package forms
  */
-class FieldList // extends \glFusion\FieldList
+class FieldList extends \glFusion\FieldList
 {
     /** Template variable.
      * @var object */
@@ -81,160 +81,63 @@ class FieldList // extends \glFusion\FieldList
 
 
     /**
-     * Create a checkbox.
+     * Create a HTML view link.
      *
-     * @param   array   $args   Array of properties
-     * @return  string      HTML for checkbox
+     * @param   array   $args   Argument array
+     * @return  string      HTML for field
      */
-    public static function checkbox($args)
+    public static function codeview($args)
     {
         $t = self::init();
-        $t->set_block('field','field-checkbox');
-
-        // Go through the required or special options
-        $t->set_block('field', 'attr', 'attributes');
-        foreach ($args as $name => $value) {
-            switch ($name) {
-            case 'checked':
-            case 'disabled':
-                if ($value) {
-                    $value = $name;
-                } else {
-                    continue 2;
-                }
-                break;
-            }
-            $t->set_var(array(
-                'name' => $name,
-                'value' => $value,
-            ) );
-            $t->parse('attributes', 'attr', true);
-        }
-        $t->parse('output', 'field-checkbox');
-        return $t->finish($t->get_var('output'));
-    }
-
-
-    /**
-     * Create a single radio button.
-     *
-     * @param   array   $args   Array of properties
-     * @return  string      HTML for ratio button
-     */
-    public static function radio($args)
-    {
-        $t = self::init();
-        $t->set_block('field','field-radio');
-
-        // Go through the required or special options
-        $t->set_block('field', 'attr', 'attributes');
-        foreach ($args as $name=>$value) {
-            switch ($name) {
-            case 'checked':
-            case 'disabled':
-                if ($value) {
-                    $value = $name;
-                } else {
-                    continue 2;
-                }
-                break;
-            }
-            $t->set_var(array(
-                'name' => $name,
-                'value' => $value,
-            ) );
-            $t->parse('attributes', 'attr', true);
-        }
-        $t->parse('output', 'field-radio');
-        return $t->finish($t->get_var('output'));
-    }
-
-
-    /**
-     * Create a selection dropdown.
-     * Options can be in a string named `option_list` or an array of
-     * separate properties.
-     *
-     *  $opts = array(
-     *      'name' => 'testoption',
-     *      'onchange' => "alert('here');",
-     *      'options' => array(
-     *          'option1' => array(
-     *              'disabled' => true,
-     *              'value' => 'value1',
-     *          ),
-     *          'option2' => array(
-     *              'selected' => 'something',
-     *              'value' => 'value2',
-     *          ),
-     *          'option3' => array(
-     *              'selected' => '',
-     *              'value' => 'XXXXX',
-     *          ),
-     *      )
-     *  );
-     *
-     *  @param  array   $args   Array of properties to use
-     *  @return string      HTML for select element
-     */
-    public static function select($args)
-    {
-        if (!isset($args['options']) && !isset($args['option_list'])) {
-            return '';
+        $t->set_block('field','field-codeview');
+        if (isset($args['url'])) {
+            $t->set_var('url', $args['url']);
+        } else {
+            $t->set_var('url','#');
         }
 
-        $t = self::init();
-        $t->set_block('field','field-select');
-
-        $def_opts = array(
-            'value' => '',
-            'selected' => false,
-            'disabed' => false,
-        );
-
-        // Create the main selection element.
-        $t->set_block('field', 'attr', 'attributes');
-        foreach ($args as $name=>$value) {
-            if ($name == 'options') {
-                // Handle the options later
-                continue;
-            } elseif ($name == 'option_list') {
-                // options were supplied as a string of <option> elements
-                $t->set_var('option_list', $value);
-            } else {
+        if (isset($args['attr']) && is_array($args['attr'])) {
+            $t->set_block('field','attr','attributes');
+            foreach($args['attr'] AS $name => $value) {
                 $t->set_var(array(
                     'name' => $name,
-                    'value' => $value,
-                ) );
+                    'value' => $value)
+                );
+                $t->parse('attributes','attr',true);
             }
-            $t->parse('attributes', 'attr', true);
+        }
+        $t->parse('output','field-codeview');
+        return $t->finish($t->get_var('output'));
+    }
+
+
+    /**
+     * Create a reset link field.
+     *
+     * @param   array   $args   Argument array
+     * @return  string      HTML for field
+     */
+    public static function reset($args)
+    {
+        $t = self::init();
+        $t->set_block('field','field-reset');
+        if (isset($args['url'])) {
+            $t->set_var('url', $args['url']);
+        } else {
+            $t->set_var('url','#');
         }
 
-        // Now loop through the options.
-        if (isset($args['options']) && is_array($args['options'])) {
-            $t->set_block('select', 'options', 'opts');
-            foreach ($args['options'] as $name=>$data) {
-                $t->set_var('opt_name', $name);
-                // Go through the required or special options
-                foreach ($def_opts as $optname=>$def_val) {
-                    if (isset($data[$optname])) {
-                        $t->set_var($optname, $data[$optname]);
-                        unset($data[$optname]);
-                    } else {
-                        $t->set_var($optname, $def_val);
-                    }
-                }
-                // Now go through the remaining supplied args for this option
-                $str = '';
-                foreach ($data as $name=>$value) {
-                    $str .= "$name=\"$value\" ";
-                }
-                $t->set_var('other', $str);
-                $t->parse('opts', 'options', true);
+        if (isset($args['attr']) && is_array($args['attr'])) {
+            $t->set_block('field','attr','attributes');
+            foreach($args['attr'] AS $name => $value) {
+                $t->set_var(array(
+                    'name' => $name,
+                    'value' => $value)
+                );
+                $t->parse('attributes','attr',true);
             }
         }
-        $t->parse('output', 'field-select');
-        $t->clear_var('opts');
+        $t->parse('output','field-reset');
         return $t->finish($t->get_var('output'));
     }
 
